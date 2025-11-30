2658ff56c54f7ce5495817049a17ff7b235e5113
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-11-30 15:42:44 -0300

    Formatted Button component code

--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { StyleSheet, View, Pressable, Text } from "react-native";
 
 type Props = {
   label: string;
@@ -7,7 +7,10 @@ type Props = {
 export default function Button({ label }: Props) {
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert("You pressed a button.")}
+      >
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
@@ -19,20 +22,20 @@ const styles = StyleSheet.create({
     width: 320,
     height: 68,
     marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 3,
   },
   button: {
     borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
   },
   buttonLabel: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
   },
 });
