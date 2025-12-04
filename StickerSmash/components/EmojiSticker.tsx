2f494b3e1132375ab786cb79ad646250cbf3eadd
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-03 21:23:52 -0300

    Adjusted a little bit the stickers' position

--- StickerSmash/components/EmojiSticker.tsx
+++ StickerSmash/components/EmojiSticker.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   return (
-    <View style={{ top: -350 }}>
+    <View style={{ top: -400, left: 25 }}>
       <Image
         source={stickerSource}
         style={{ width: imageSize, height: imageSize }}
